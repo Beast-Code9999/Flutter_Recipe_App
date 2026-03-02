04a583cb0ba183810f0ca659490d928247604247
beastcode9999 <beastcode9999@gmail.com>
2026-03-02 15:30:47 +0930

    feat: Add boilerplate and inital starting files

---
+++ recipe_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
